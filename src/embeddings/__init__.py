e8c89e7da76f49f47266ac5c194c14be2e326233
Igor <romanchaa997@gmail.com>
2025-12-21 21:05:03 +0200

    Add embeddings module __init__.py

---
+++ src/embeddings/__init__.py
@@ -0,0 +1,13 @@
+"""Embeddings module for vector representation of professional profiles."""
+
+from .core import (
+    EmbeddingConfig,
+    EmbeddingGenerator,
+    ProfileEmbedder,
+)
+
+__all__ = [
+    "EmbeddingConfig",
+    "EmbeddingGenerator",
+    "ProfileEmbedder",
+]
