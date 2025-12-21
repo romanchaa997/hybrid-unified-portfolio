abe53e524ac9b0f9ee9fa684f3f5b9683de03ac1
Igor <romanchaa997@gmail.com>
2025-12-21 21:10:02 +0200

    Update src/__init__.py to include semantic_search module

--- src/__init__.py
+++ src/__init__.py
@@ -6,10 +6,12 @@ __email__ = "romanchaa997@gmail.com"
 
 from . import embeddings
 from . import skill_extraction
+from . import semantic_search
 
 __all__ = [
     "embeddings",
     "skill_extraction",
+        "semantic_search",
     "__version__",
     "__author__",
     "__email__",
